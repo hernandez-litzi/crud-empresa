bc0d08cd7d79a026597d1ea6f00217a40dd19483
Litzi <hernandezlitzi40@gmail.com>
2026-01-21 12:47:42 -0600

    Prueba

--- src/empleados/dto/create-empleados.dto.ts
+++ src/empleados/dto/create-empleados.dto.ts
@@ -1,15 +1,6 @@
-import { IsString, IsEmail } from 'class-validator';
-
 export class CreateEmpleadoDto {
-  @IsString()
   nombre: string;
-
-  @IsString()
   apellido: string;
-
-  @IsString()
   telefono: string;
-
-  @IsEmail()
   correo: string;
 }
